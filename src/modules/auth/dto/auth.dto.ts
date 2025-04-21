d87574ae822fe21bcb0e258992f6f28146aadf61
= <b.b.albayrak@hotmail.com>
2025-04-22 00:31:39 +0300

    Feature: Add validation for email and password fields in AuthDto

--- src/modules/auth/dto/auth.dto.ts
+++ src/modules/auth/dto/auth.dto.ts
@@ -1,12 +1,18 @@
-import { IsString } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class AuthDto {
   @IsString({
     message: 'Email must be a string',
   })
+  @IsNotEmpty({
+    message: 'Email is required',
+  })
   private readonly email: string;
   @IsString({
     message: 'Password must be a string',
+  })
+  @IsNotEmpty({
+    message: 'Email is required',
   })
   private readonly password: string;
 }
