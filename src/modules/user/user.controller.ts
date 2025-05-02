69e6ff042e3a8cc79c03c90f8c8eab06e099e6cf
= <b.b.albayrak@hotmail.com>
2025-05-02 22:52:45 +0300

    feat: add user retrieval, update, and deletion functionalities in UserController and UserService

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,7 +1,11 @@
 import {
+  Body,
   Controller,
+  Delete,
   Get,
   HttpStatus,
+  Param,
+  Put,
   Req,
   Res,
   UseGuards,
@@ -11,6 +15,8 @@ import { JwtAuthGuard } from '../auth/passport/jwt.guard';
 import { RolesGuard } from 'src/decorators/roles.guard';
 import { Roles } from 'src/decorators/roles.decorator';
 import { UserStatus } from './userType';
+import { UserDto } from './dto/user.dto';
+import { Request, Response } from 'express';
 
 @UseGuards(JwtAuthGuard, RolesGuard)
 @Controller('user')
@@ -38,6 +44,67 @@ export class UserController {
     return res.status(HttpStatus.OK).json({
       message: 'All users retrieved successfully',
       data: users,
+    });
+  }
+
+  @Get(':id')
+  async getUserById(@Param('id') id: number, @Res() res: any) {
+    const user = await this.userService.getUserById(id);
+    if (!user) {
+      return res.status(HttpStatus.NOT_FOUND).json({
+        message: 'User not found',
+      });
+    }
+    return res.status(HttpStatus.OK).json({
+      message: 'User retrieved successfully',
+      data: user,
+    });
+  }
+
+  @Put(':id')
+  async updateUserById(
+    @Body() user: UserDto,
+    @Param('id') id: number,
+    @Req() req: Request,
+    @Res() res: Response,
+  ) {
+    if (!req.body) {
+      return res.status(HttpStatus.BAD_REQUEST).json({
+        message: 'No data provided for update',
+      });
+    }
+
+    const updatedUser = await this.userService.updateUser(id, user);
+    return res.status(HttpStatus.CREATED).json({
+      message: 'User updated successfully',
+      data: updatedUser,
+    });
+  }
+
+  @Delete(':id')
+  async deleteUserById(
+    @Param('id') id: number,
+    @Req() req: Request,
+    @Res() res: Response,
+  ) {
+    const userId = req.user['userId'];
+    const role = req.user['role'];
+
+    if (role !== UserStatus.ADMIN && userId !== id) {
+      return res.status(HttpStatus.FORBIDDEN).json({
+        message: 'You are not authorized to delete this user',
+      });
+    }
+
+    const userExists = await this.userService.getUserById(id);
+    if (!userExists) {
+      return res.status(HttpStatus.NOT_FOUND).json({
+        message: 'User not found',
+      });
+    }
+    await this.userService.deleteUser(id);
+    return res.status(HttpStatus.OK).json({
+      message: 'User deleted successfully',
     });
   }
 }
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { USER_REPOSITORY } from 'src/config/constants';
 import { User } from './user.entity';
 import { UserDto } from './dto/user.dto';
@@ -46,5 +46,31 @@ export class UserService {
       where: { id: user_id },
     });
     return user;
+  }
+
+  async updateUser(user_id: number, user: UserDto): Promise<User | null> {
+    const existingUser = await this.userRepository.findOne<User>({
+      where: { id: user_id },
+    });
+    if (!existingUser) {
+      throw new NotFoundException('User not found');
+    }
+    const hashedPassword = await bcrypt.hash(user.password, 12);
+    const hashedUser = {
+      ...user,
+      password: hashedPassword,
+    };
+
+    const updatedUser = await existingUser.update(hashedUser, {
+      where: { id: user_id },
+    });
+    return updatedUser;
+  }
+
+  async deleteUser(user_id: number): Promise<void> {
+    const user = await this.userRepository.findOne<User>({
+      where: { id: user_id },
+    });
+    await user.destroy();
   }
 }
