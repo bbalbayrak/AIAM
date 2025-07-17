765a470719c917c6933d716bff4a05e4f69a51e9
= <b.b.albayrak@hotmail.com>
2025-07-18 01:44:31 +0300

    feat: configure Docker setup with Dockerfile and docker-compose for backend, database, and RabbitMQ services

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
   app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.RMQ,
     options: {
-      urls: ['amqp://localhost:5672'],
+      urls: [process.env.RABBITMQ_URL || 'amqp://localhost:5672'],
       queue: 'messages_queue',
       queueOptions: {
         durable: true,
